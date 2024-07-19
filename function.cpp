bede9580b7c0f887799febddeba3bd46b9f1a0bc
AshleyMDickson <ashley.dickson.23@ucl.ac.uk>
2024-07-19 22:50:44 +0100

    Primality testing

--- function.cpp
+++ function.cpp
@@ -5,13 +5,24 @@ using namespace std;
 
 int f(int a, int b)
 {
-    vector<int> factors{};
+    vector<int> prime_list{};
 
     for(int i = a; i < b; ++i)
     {
-        
-        factors.push_back(i);
+        int factor_count = 0;
+        for(int j = 2; j < i/2; ++j){
+            if(i%j == 0){
+                ++factor_count;
+            }
+            //return factor_count;
+        }
+        if(factor_count == 0){
+            prime_list.push_back(i);
+        }
     }
-
+    for(int i: prime_list){
+        cout << i << " ";
+    }
+    cout << endl;
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
-// Template C++ program file
-//#include <iostream>
-
 // include our header file with the function declaration
 #include "function.h" 
+#include <iostream>
 
-//using namespace std;
+using namespace std;
 
 int main()
 {
-    f(7, 112);
+    char z{'y'};
+    while(z = 'y'){
 
+        int x{}; int y{}; 
+        cout << "Enter your first number: " << endl;
+        cin >> x;
+        cout << "Enter your second number" << endl;
+        cin >> y;
+        cout << "Here's the list of primes between your two numbers:" << endl;
+        f(x, y);
+        cout << "Run it again? (y/n):" << endl;
+        cin >> z;
+        if(z == 'y'){
+        } else {
+            z = 'n';
+            exit;
+        }
+    }
     return 0;
 }
